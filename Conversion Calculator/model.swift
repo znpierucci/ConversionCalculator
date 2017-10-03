740d5c9658b8fb5f9e2e0e7c1cc9aec45c64d43b
Benjarit <bhmcf@mail.missouri.edu>
2017-10-03 13:53:53 -0500

    Created a new swift file

---
+++ Conversion Calculator/model.swift
@@ -0,0 +1,9 @@
+//
+//  model.swift
+//  Conversion Calculator
+//
+//  Created by Benjarit Hotrabhavananda on 10/3/17.
+//  Copyright © 2017 Zachary Pierucci. All rights reserved.
+//
+
+import Foundation
